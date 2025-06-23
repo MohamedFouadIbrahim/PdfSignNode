2879d46c6031872ed6233b25078e71ae5d164d33
mohamedfouadrn <mohamed.fouadrn@gmail.com>
2025-06-23 12:18:00 +0400

    Port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { mergeSignPDF } = require('./signPDF');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Enable CORS for all routes
 app.use(cors());
